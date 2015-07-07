e090639d4daeb7812ec8fb59640bef85f6d8ac59
Kyle Snow Schwartz <kyle.snowschwartz@gmail.com>
2015-07-07 18:50:07 +1200

    is ordering to find high scores working? not really sure

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,5 +2,6 @@ class PlayersController < ApplicationController
 	before_action :authenticate_player!
 
 	def index
-	end	
+		@top_10_players = Player.includes(:scores).order("scores.points desc").last(10)
+	end
 end
