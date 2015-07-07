a88ef80aa514aebd828ed61b6dd1c5cd592de9c4
Kyle Snow Schwartz <kyle.snowschwartz@gmail.com>
2015-07-07 16:01:45 +1200

    tests.... ohhhhhh the greeeennnnnnnn sweet sweet greeeennnn

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -4,12 +4,17 @@ RSpec.describe Player, type: :model do
   let(:valid_player) { create(:player)}
   let(:invalid_player) { create(:player, email: "Jeffrey", password: true)}
 
-describe "fields" do
-	it { should have_db_column(:name).of_type(:string) }
-	it { should have_db_column(:email).of_type(:string) }
-	it { should have_db_column(:encrypted_password).of_type(:string) }
-end
+	describe "fields" do
+		it { should have_db_column(:name).of_type(:string) }
+		it { should have_db_column(:email).of_type(:string) }
+		it { should have_db_column(:encrypted_password).of_type(:string) }
+	end
 
+	describe "validations" do
+		it { should validate_presence_of(:name)}
+		it { should validate_presence_of(:email)}
+		it { should validate_presence_of(:encrypted_password)}
+	end
 
 
 
