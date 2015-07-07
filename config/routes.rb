fcd08329287ebe8d55e43229cc368478267faf21
Kyle Snow Schwartz <kyle.snowschwartz@gmail.com>
2015-07-07 17:57:36 +1200

    if it works, then i gues it... uhh

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :players
+  devise_for :players, :controllers => {:registrations => "registrations"}
   root to: "players#index"
 
   resources :players, except: [ :update, :edit, :destroy ] do
