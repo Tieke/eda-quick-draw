6e00b7f99a9170e294d3601438ade619f70e04ae
Kyle Snow Schwartz <kyle.snowschwartz@gmail.com>
2015-07-07 16:48:32 +1200

    routes bitches

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :players
   root to: "welcome#index"
 
+  resources :players, except: [ :update, :edit, :destroy ] do
+    resources :scores, only: [ :index, :create ]
+  end
+
   
 end
