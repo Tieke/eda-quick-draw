8a58d69a29f2e10cab9ba9b31d7a346b24131f14
Kyle Snow Schwartz <kyle.snowschwartz@gmail.com>
2015-07-07 16:39:00 +1200

    devise doesn't want to through errors when you fuck up, apparently

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 Player.destroy_all
 
 10.times do 
-	player = Player.create(name: Faker::Name.name, email: Faker::Internet.email, encrypted_password: Faker::Internet.password(8))
+	player = Player.create(name: Faker::Name.name, email: Faker::Internet.email, password: Faker::Internet.password(8))
 	10.times {player.scores << Score.create(points: rand(10..100))}
 end
