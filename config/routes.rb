6832f04894afda9b7b60eec02da616f6c62cd181
Kyle Snow Schwartz <kyle.snowschwartz@gmail.com>
2015-07-07 15:47:54 +1200

    made score and player migrations and models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :players
   root to: "welcome#index"
+
+  
 end
---
+++ spec/factories/scores.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :score do
+    points 1
+player nil
+  end
+
+end
