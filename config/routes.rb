48e8cd1bed16311b1595deab2720b3e094716996
Te Aihe Butler <teaihebutler@gmail.com>
2015-07-09 17:42:47 +1200

    messing around with logo positioning on index page and sign in page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :players, :controllers => {:registrations => "registrations"}
+  devise_for :players, :path => '', :controllers => {:registrations => "registrations"}
   root to: "players#index"
 
   resources :players, except: [ :update, :edit, :destroy ] do
