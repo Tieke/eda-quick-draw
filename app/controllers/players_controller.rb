df2475e92574d50d57468e05623b140edee6fce0
Te Aihe Butler <teaihebutler@gmail.com>
2015-07-09 17:11:32 +1200

    skipped authenticity check in scores-controller. plus other shit

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,8 +3,10 @@ class PlayersController < ApplicationController
 
 	def index
 		@top_10_players = Player.joins(:scores).order("scores.points desc").pluck(:name, :points).uniq{|player| player[0]}.first(10)
-		last_score = current_player.scores.last.points || 0
-		high_score = current_player.high_score || 0
-		@message = [last_score, high_score]
+		if current_player.scores
+			last_score = current_player.scores.last.points 
+			high_score = current_player.high_score 
+			@message = [last_score, high_score]
+		end
 	end
 end
