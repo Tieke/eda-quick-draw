5e9df85c42d93fd71b8b01a001b97642c49a86d3
Kyle Snow Schwartz <kyle.snowschwartz@gmail.com>
2015-07-10 15:45:04 +1200

    back to 60

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -288,7 +288,7 @@ function sendPoints(scoreObject) {
 function updateTime()
 {
     gameTime += 1;
-    if(gameTime > 5)
+    if(gameTime > 60)
     {
         //End Game and Clean up
         timerText.text = "fin";
